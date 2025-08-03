7fddb4d8a267a0e095edd9b7eaad37fce0ccf3c1
Diego Palma <dpalma@symbotic.com>
2025-08-03 11:59:55 -0500

    Create get_data

--- scripts/__init__.py
+++ scripts/__init__.py
@@ -1,5 +1,5 @@
 from .CamParameters import CameraParameters
 from .datatypes import Rod
 from .tracker import Tracker
-from .utils import plot_historic, read_yaml_file, get_positions
+from .utils import plot_historic, read_yaml_file, get_positions, get_data
 from .logger import Logger
